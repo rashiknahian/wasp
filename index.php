622774a6a4fc0240c2a3f066894fbb45d3747217
rashiknahian <rashik@nahian.me>
2020-04-20 12:10:34 +0600

    version - 1.00

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include("head.php");?>
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <?php include("nav.php");?>
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 ">
+                
+            </div>
+        </div>
+    </div>
+    <?php include("footer.php");?>
+    <?php include("script.php");?>
+
+</body>
+</html>
---
+++ script.php
@@ -0,0 +1,5 @@
+    <!-- Custom Scripts -->
+    <script src="js/script.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script src="js/jquery.slim.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
